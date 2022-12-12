c9c99fbe41c875a55ebb48c7193dbdd7278c4726
abenaTenkorang <abenagy16@gmail.com>
2022-12-12 08:32:41 +0000

    Setup webpack config

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+﻿const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+ module.exports = {
+    mode: 'production',
+   entry: {
+     index: './src/index.js',
+   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+   output: {
+     filename: '[name].js',
+     path: path.resolve(__dirname, 'dist'),
+     clean: true,
+   },
+   devServer: {
+    static: './dist',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+ };
